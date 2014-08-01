4bff8baa4540ad6b7193cc703e8cbaa7fc4b0699
Matt <matt@thebaileyresidence.co.uk>
2014-08-01 22:07:31 +0100

    Added exercise 7.

---
+++ ex7.py
@@ -0,0 +1,24 @@
+# Print mary had a little lamb to the console
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+# Print 10 full stops
+print "." * 10 # what'd that do?
+
+# Initialize variables with characters spelling out CheeseBurger
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Print the variables in order to the console, the comma does not introduce a line break
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
