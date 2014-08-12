7879a84a9893b7be4368c1ccdd04d0f2ef948393
Matt <matt@thebaileyresidence.co.uk>
2014-08-12 21:09:40 +0100

    Added exercise 13.

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second, third = argv
+
+name = raw_input("What is your name?")
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "Your names is:", name
